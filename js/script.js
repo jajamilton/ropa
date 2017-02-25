d7d6499e9ffd8b4ca1fa75319a5b28cc4076ec4f
jajamilton <hlopez415@misena.edu.co>
2017-02-25 15:16:13 -0500

    plantilla 0.1
    parte incial del inicio,falta arreglar el responsive

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//-------Imagenes que rotan------------------------------------------------------------------
+		var config={};
+    config.width =550;  //slider width size in pixels 
+    config.height=350;  //slider height size in pixels 
+    config.intervalTime  =7000; //mili-seconds between slides   
+    $('.slider').cleanSlider(config);
